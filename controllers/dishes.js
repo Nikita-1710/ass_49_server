95a4bce96e1bf82fdb202684ffa74edab6ba5c90
Nikita-1710 <chausalkarnikita@gmail.com>
2025-06-15 16:42:41 +0530

    server for products API created

---
+++ controllers/dishes.js
@@ -0,0 +1,172 @@
+// Controllers in Seperate files
+
+// this is a temporary data store
+const DISHES = [
+    {
+        "id": 1,
+        "name": "Panner",
+        "category": "Veg"
+    },
+    {
+        "id": 2,
+        "name": "Chicken",
+        "category": "Non-Veg"
+    }
+];
+
+const getDishes = (req, res) => {
+    res.json({
+        success: true,
+        data: DISHES,
+        message: "Dishes fetched successfully"
+    });
+}
+
+const postDishes = (req, res) => {
+    const { name, category, id } = req.body
+
+    // validation for existing id
+    for (const dish of DISHES) {
+        if (id === dish.id) {
+            return res.status(400).json({
+                success: false,
+                message: "ID already exists."
+            })
+        }
+    }
+
+    // validation for required fields
+    if (!name) {
+        return res.status(400).json({
+            success: false,
+            message: "Name is required"
+        })
+    }
+    if (!category) {
+        return res.status(400).json({
+            success: false,
+            message: "Category is required"
+        })
+    }
+    if (!id) {
+        return res.status(400).json({
+            success: false,
+            message: "ID is required"
+        })
+    }
+
+    const dishObj = {
+        id,
+        name,
+        category
+    }
+
+    DISHES.push(dishObj)
+
+    res.status(201).json({
+        success: true,
+        data: dishObj,
+        message: "Dish created successfully"
+    });
+}
+
+const deleteDishessById = (req, res) => {
+
+    const { id } = req.params
+
+    let dishIndex = -1
+
+    DISHES.forEach((dish, i) => {
+        if (dish.id == id) {
+            dishIndex = i;
+        }
+    })
+
+    if (dishIndex == -1) {
+        return res.status(404).json({
+            success: false,
+            message: `Dish with id: ${id} does not exist`
+        })
+    }
+    else {
+        DISHES.splice(dishIndex, 1)
+        return res.status(200).json({
+            success: true,
+            message: `Dish with id: ${id} deleted successfully`
+        })
+    }
+}
+
+const getDishesById = (req, res) => {
+    const { id } = req.params
+
+    let dishIndex = -1
+
+    DISHES.forEach((dish, i) => {
+        if (dish.id == id) {
+            dishIndex = i
+        }
+    })
+
+    if (dishIndex == -1) {
+        return res.status(404).json({
+            success: false,
+            message: `Dish with id: ${id} does not exist`
+        })
+    }
+    else {
+        return res.status(200).json({
+            success: true,
+            data: DISHES[dishIndex],
+            message: `Dish with id: ${id} fetched successfully`
+        })
+    }
+}
+
+const putDishessById = (req, res) => {
+    const { id } = req.params
+    const { name, category } = req.body
+
+    // validation for required fields
+    if (!name) {
+        return res.json({
+            success: false,
+            message: "Name is required"
+        })
+    }
+    if (!category) {
+        return res.json({
+            success: false,
+            message: "Category is required"
+        })
+    }
+
+    let dishIndex = -1
+
+    DISHES.forEach((dish, i) => {
+        if (dish.id == id) {
+            dishIndex = i;
+        }
+    })
+
+    if (dishIndex == -1) {
+        return res.json({
+            success: false,
+            message: `Dish with id: ${id} does not exists`
+        })
+    }
+
+    DISHES[dishIndex] = {
+        id: parseInt(id),
+        name: name,
+        category: category
+    }
+
+    res.json({
+        success: true,
+        data: DISHES[dishIndex],
+        message: `Dish with id: ${id} updated successfully`,
+    })
+}
+
+export { getDishes, postDishes, deleteDishessById, getDishesById, putDishessById }
--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-import express from express;
+import express from 'express';
+import cors from 'cors'
+import { getDishes, postDishes, deleteDishessById, getDishesById, putDishessById, } from './controllers/dishes.js';
 
-const app = express()
+const app = express();
 
+app.use(cors())
+app.use(express.json());
+
+const PORT = 5002;
+
+app.get("/dishes", getDishes);  // API for read dishes
+app.post("/dishes", postDishes) // API for create dishes
+app.delete("/dishes/:id", deleteDishessById) // API for delete dishes giving ID
+app.get("/dishes/:id", getDishesById)   // API for get dish details by ID
+app.put("/dishes/:id", putDishessById)   // API for update dishes component
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
